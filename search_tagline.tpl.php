feeed2a8bf80275f01e0f410d918b0108b9e8506
tanmancan <contact@tkarimdesign.com>
2016-08-22 09:52:57 -0400

    updated template and styles

--- search_tagline.tpl.php
+++ search_tagline.tpl.php
@@ -1,1 +1,11 @@
-<h1>TEST</h1>
+<?php if($tagline_one || $tagline_two): ?>
+  <div class="slideshow-slogan">
+    <?php if($tagline_one): ?>
+      <div class="slogan-primary">Come Experience The Mountain</div>
+    <?php endif; ?>
+    <?php if($tagline_two): ?>
+      <div class="slogan-secondary">And Enjoy Personalized Service From Alta Chalets</div>
+    <?php endif; ?>
+  </div>
+  <!-- /.slideshow-slogan -->
+<?php endif;?>
